ab8fd0bf2425eb4d7d22c110c9c021fbc4c02cc4
davewood <davwood_1@yahoo.com.au>
2013-09-09 14:26:44 +0100

    Player updates

--- lib/player.rb
+++ lib/player.rb
@@ -2,20 +2,21 @@ class Player
 
 	def initialize(name)
 	    @name = name
-	    @board = [['s', 's', '', 's', '', '', 's', 's', 's', 's']]
 	end 
 
 	def name
 		@name
 	end   
 
-	def has_ships_still_floating?
-		puts @board.include?("s")
-		puts @board.inspect
+	def has_ships_still_floating?(my_board)
+		@flat_board = my_board.flatten
+		@flat_board.include?('s')
 	end
 
-	def shoots_at_the_opponents_board
+	def shoots(at_coordinates, opponent_board)
+		opponent_board.register_shot(at_coordinates)
 	end 
 
+	
 
 end 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,22 +7,26 @@ describe Player do
 	# let(:board) {double :board [['s', 's', '', 's', '', '', 's', 's', 's', 's']]}
 	
 	it 'should initialize with a new player named Dave' do
-		
 		expect(player.name).to eq("Dave")
 	end
 
 	it 'will look the player\'s board and return true if ships are floating' do
-		board = double([['s', 's', '', 's', '', '', 's', 's', 's', 's']])
-		expect(:board.each {|x| x == "s"}).to be_true
-		dave.has_ships_still_floating?
+		my_board = [['s', 's', '', 's', '', '', 's', 's', 's', 's']]
+		@flat_board = my_board.flatten
+		expect(@flat_board.include?('s')).to be_true
+	end
+
+	it 'will return false when there are are no ships floating' do
+		my_board = [['x', 'x', '', 'x', '', '', 'x', 'x', 'x', 'x']]
+		expect(player.has_ships_still_floating?(my_board)).to be_false
 	end
 
 	it 'will shoot at the opponents board' do
-		board = double :board
-
+		opponent_board = double :opponent_board
+		at_coordinates = "A1"
+		opponent_board.should_receive(:register_shot).with(at_coordinates)
+		player.shoots(at_coordinates, opponent_board)
 	end
-
-
 
 
 end
